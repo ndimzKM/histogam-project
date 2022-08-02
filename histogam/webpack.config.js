495db655cb388599dc9dcc01c9baae5c4d3db899
Alieu Ndimbalane <alieundimbalane@gmail.com>
2022-08-02 20:20:54 +0000

    first commit

---
+++ histogam/webpack.config.js
@@ -0,0 +1,39 @@
+const production = process.env.NODE_ENV === "production";
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: production ? 'production' : 'development',
+    entry: './src/index.tsx',
+    output: {
+        path: __dirname + '/dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                resolve: {
+                    extensions: ['.ts', '.tsx', '.js', '.json'],
+                },
+                use: 'ts-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ]
+    },
+    devtool: production ? undefined : 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+};
