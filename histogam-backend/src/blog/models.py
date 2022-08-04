ab0bd6fa23f5af430a7dd94db4a3738861198dd4
jattanjie21 <laboyjatta@gmail.com>
2022-08-04 14:44:49 +0000

    Added user to posts

--- histogam-backend/src/blog/models.py
+++ histogam-backend/src/blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 class Post(models.Model):
     title      = models.CharField(max_length=200)
     content    = models.TextField()
     image      = models.ImageField(upload_to='images/', blank=True, null=True)
+    posted_by  = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- histogam-backend/src/blog/mutations.py
+++ histogam-backend/src/blog/mutations.py
@@ -5,10 +5,26 @@ from .queries import PostType
 from .forms import PostForm
 
 
-class CreatePost(DjangoModelFormMutation):
-    class Meta:
-        form_class        = PostForm
-        model             = Post
-        return_field_name = 'post'
+class CreatePost(Mutation):
+    create_post = Field(PostType)
+
+    class Arguments:
+        title   = String(required=True)
+        content = String(required=True)
+        image   = String()
+
+    def mutate(self, info, title, content, image):
+
+        user = info.context.user
+
+        if user.is_anonymous:
+            raise Exception('Log in to post content')
+
+        post = Post(title=title, content=content, image=image, posted_by=user)
+
+        post.save()
+
+        return CreatePost(create_post=post)
+
 
 
