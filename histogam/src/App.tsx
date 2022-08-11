9465789cb6354189a0d250b9d14bed151379e5c6
Alieu Ndimbalane <alieundimbalane@gmail.com>
2022-08-11 00:53:31 +0000

    done some refactoring after merging the two repos

--- histogam/src/App.tsx
+++ histogam/src/App.tsx
@@ -1,11 +1,48 @@
-import React from "react";
+import * as React from 'react';
+import Register from './auth/Register';
+import Login from './auth/Login';
+import Home from './pages/Home';
+import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth'
 
-const App = () => {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    )
+
+function App() {
+
+  const [user, setUser] = React.useState<any>(null);
+  const [authState , setAuthState] = React.useState<any>(null);
+ 
+     React.useEffect(() => {
+    const unSubscribeAuth= onAuthStateChanged(auth,
+       async authenticatedUser => {
+        if (authenticatedUser) {
+          setUser(authenticatedUser.email);
+          setAuthState('home');
+        } else {
+          setUser(null);
+          setAuthState('login'); // or 'register' 
+        } })
+        return unSubscribeAuth();
+
+      
+      }, [user]);
+
+          
+      if(authState === 'null') return <div className='font-bold text-center text-5xl'>Loading...</div>
+        
+      if(authState === 'login') return <Login {...setAuthState} {...setUser}/>
+      if(authState === 'register') return <Register {...setAuthState} {...setUser} />
+        
+    if(user){
+        return <Home {...setAuthState}/>
+
+    }
+        
+  return (
+
+  
+      <Register />
+      
+  )
 }
 
 export default App;
--- histogam/src/components/Form.tsx
+++ histogam/src/components/Form.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 
-export default function Register({
-
-
-}) {
+export default function Form() {
     return (
         <div className=' w-11/12 max-w-[700px] px-10 py-20 rounded-3xl bg-white border-2 border-gray-100'>
-            <h1 className='text-5xl font-semibold text-center'>Register</h1>
-            <p className='font-medium text-lg text-center text-gray-500 mt-4'>please sign up to get started.</p>
+            <h1 className='text-5xl font-semibold text-center'>Welcome to the history of The Gambia</h1>
+            <p className='font-medium text-lg text-center text-gray-500 mt-4'>please sign in or up to get started.</p>
             <div className='mt-8'>
                 <div className='flex flex-col'>
                     <label className='text-lg font-medium'>Email</label>
@@ -19,7 +16,7 @@ export default function Register({
                     <label className='text-lg font-medium'>Password</label>
                     <input 
                         className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
-                        placeholder="Enter your email"
+                        placeholder="Enter your password"
                         type={"password"}
                     />
                 </div>
@@ -43,10 +40,12 @@ export default function Register({
                             Sign in with Google
                     </button>
                 </div>
-               
+                <div className='mt-8 flex justify-center items-center'>
+                    <p className='font-medium text-base'>Don't have an account?</p>
+                    <button 
+                        className='ml-2 font-medium text-base text-violet-500'>Sign up</button>
+                </div>
             </div>
-            
         </div>
-        
     )
 }
